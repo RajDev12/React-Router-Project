54064ee4f8c9b5ff972b19623942ce955191f622
RajDev12 <rjnath98@gmail.com>
2024-08-26 00:51:58 +0530

    chaining routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,45 @@ import App from './App.jsx'
 import './index.css' 
 import { createBrowserRouter, RouterProvider  } from 'react-router-dom'
 import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import { createRoutesFromElements } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 const router1 =createBrowserRouter([
   {path: "/",
-    element: <Layout/>
+    element: <Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"Contact",
+        element:<Contact/>
+      },
+      {path:"About",
+        element:<About/>}
+    ]
   }
 ])
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout />}>
+//       <Route path='' element={<Home />} />
+//       <Route path='about' element={<About />} />
+//       <Route path='contact' element={<Contact />} />
+//       <Route path='user/:userid' element={<User />} />
+//       <Route 
+//       loader={githubInfoLoader}
+//       path='github' 
+//       element={<Github />}
+//        />
+//     </Route>
+//   )
+// )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
